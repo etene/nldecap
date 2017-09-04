4cca933fba38dbe633e2417f96b699f6c6b6cdfc
Étienne Noss <etienne.noss@wifirst.fr>
2017-09-04 22:15:41 +0200

    tests: add tests for nldecap.py usage as a script

--- tests/nldecap_tests.py
+++ tests/nldecap_tests.py
@@ -12,11 +12,16 @@ PCAPS_DIR = os.path.join(os.path.dirname(sys.modules[__name__].__file__),
                          "pcaps")
 
 
+def get_pcap_path(name):
+    """Given a pcap's name in the test directory, returns its full path."""
+    return os.path.join(PCAPS_DIR, name)
+
+
 @contextmanager
 def open_pcap(name):
     """Given a pcap's name in the test directory, returns a file handle to it.
     """
-    fullpath = os.path.join(PCAPS_DIR, name)
+    fullpath = get_pcap_path(name)
     fd = open(fullpath, "rb")
     yield fd
     fd.close()
@@ -24,7 +29,7 @@ def open_pcap(name):
 
 class NLPcapTests(unittest.TestCase):
     """Tests the NLPcap class"""
-    def test_valid_1pkt(self):
+    def test_valid_pcap(self):
         """Valid big and little endian pcaps must be properly parsed"""
         for pcapfile in "two_packets_be.pcap", "two_packets_le.pcap":
             with open_pcap(pcapfile) as fd:
@@ -61,6 +66,41 @@ class NLPcapTests(unittest.TestCase):
         with open_pcap("garbage") as fd:
             with self.assertRaises(nldecap.PcapError):
                 _ = nldecap.NLPcap(fd)
+
+
+class ScriptTests(unittest.TestCase):
+    """Tests calls to main() (when called as a script)"""
+    @staticmethod
+    def call_main(*args):
+        """shorthand for nldecap.main()
+        TODO: redirect std{in,out,err}
+        """
+        return nldecap.main(args)
+
+    def test_basic_usage(self):
+        """main() called with a simple pcap must return 0"""
+        ret = self.call_main(get_pcap_path("two_packets_le.pcap"))
+        self.assertEquals(ret, 0)
+
+    def test_non_netlink_pcap(self):
+        """main() called with a non-netlink pcap must exit unsuccessfully"""
+        with self.assertRaises(SystemExit):
+            self.call_main(get_pcap_path("1pkt_not_netlink_le.pcap"))
+
+    def test_no_args(self):
+        """main() called without arguments must exit unsuccessfully"""
+        with self.assertRaises(SystemExit):
+            self.call_main()
+
+    def test_filter(self):
+        """main() called with a valid pcap and filter must return 0"""
+        with self.assertRaises(SystemExit):
+            self.call_main(get_pcap_path("two_packets_le.pcap"), "rtnl")
+
+    def test_bad_filter(self):
+        """main() called with an invalid filter must exit unsuccessfully"""
+        with self.assertRaises(SystemExit):
+            self.call_main(get_pcap_path("two_packets_le.pcap"), "yolo")
 
 
 if __name__ == '__main__':
